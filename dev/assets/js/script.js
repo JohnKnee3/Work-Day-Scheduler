e9ee2377c60633f01a434993d937d00978b4f818
john.a.clark3 <john.a.clark3@gmail.com>
2022-01-12 09:37:34 -0700

    bootstrap & jquery links
    5.1.5 more just broad strokes about jquery

---
+++ dev/assets/js/script.js
@@ -0,0 +1,35 @@
+// Implement third-party libraries such as Bootstrap, jQuery, and Moment.js
+
+// Perform DOM traversals and manipulations with jQuery
+
+// Use jQuery to save user input in local storage
+
+// Work with the Bootstrap grid layout in HTML
+
+// Work with Bootstrap components
+
+// Add custom color styles to an existing CSS framework
+
+// Work with custom fonts and icons via a content delivery network (CDN)
+
+
+
+// Explain the difference between jQuery and JavaScript
+
+// Explain the difference between CSS and Bootstrap
+
+// Explain and implement libraries with content delivery networks and static files
+
+// Perform DOM traversals and manipulations with jQuery
+
+// Explain how to resolve issues related to dynamically generated HTML using jQuery
+
+// Explain and implement the Bootstrap grid layout in HTML
+
+// Integrate Bootstrap components in HTML files to quickly build user interfaces
+
+// Add custom styling to a CSS framework
+
+// Implement custom fonts and icons via CDNs
+
+//5.1.5 Intorduced a ton of Jquery ideas.  More just broadstrokes.
